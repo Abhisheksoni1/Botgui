a1f1cb72be8a985b755e22811dfc5697c9b7ecc6
abhisheksoni <abhisheksoni500@gmail.com>
2018-06-21 14:00:34 +0530

    postgres connection update

--- Botgui/settings.py
+++ Botgui/settings.py
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'Botgui.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tradebot',
+        'USER': 'django',
+        'PASSWORD': 'tradebot@123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Internationalization
